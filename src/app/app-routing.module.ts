ae4d34c0b05091eb6178c96bdbfc740ad9d28e98
Neel Narvankar <59487334+neelnarvankar30@users.noreply.github.com>
2020-04-18 01:07:40 -0400

    neel commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SurveyListComponent } from './survey-list/survey-list.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SurveyFormComponent } from './survey-form/survey-form.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: WelcomeComponent },
+  { path: 'form', component: SurveyFormComponent },
+  { path: 'display', component: SurveyListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SurveyFormComponent } from './survey-form/survey-form.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { SurveyListComponent } from './survey-list/survey-list.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './/app-routing.module';
+//import { FormComponent } from './survey-form/survey-form.component';
+import { DisplayService } from './displayservice.service';
 
+import { HttpClientModule } from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
@@ -17,6 +21,7 @@ const appRoutes: Routes = [
 
 
 @NgModule({
+  
   declarations: [
     AppComponent,
     SurveyFormComponent,
@@ -25,13 +30,16 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     ReactiveFormsModule,
     AppRoutingModule,
     RouterModule.forRoot(
       appRoutes
     )
   ],
-  providers: [],
+  
+  providers: [DisplayService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
