e521d6993c24e3c3e5de6e33c3d3a041b63caa2e
Neel Narvankar <59487334+neelnarvankar30@users.noreply.github.com>
2020-04-30 13:11:36 -0400

    New Server

--- src/app/displayservice.service.ts
+++ src/app/displayservice.service.ts
@@ -18,7 +18,7 @@ export class DisplayService {
 
    public getNews(){
     console.log("insidegeNews");
-    return this.httpClient.get('http://ec2-3-90-139-158.compute-1.amazonaws.com/jersey-quickstart-webapp/webapi/myresource');
+    return this.httpClient.get('http://ec2-3-86-94-214.compute-1.amazonaws.com/jersey-quickstart-webapp/webapi/myresource');
     
   }
 
